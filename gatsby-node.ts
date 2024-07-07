83ca23f25e43250a00c71aa2a7fba4328a9d9a2c
Giannis Dalakouras <gdala2389@gmail.com>
2024-07-07 19:23:19 +0300

    Updates

--- gatsby-node.ts
+++
@@ -1,8 +0,0 @@
-// turn off source-maps
-exports.onCreateWebpackConfig = ({ actions, stage }: any) => {
-    if (stage === 'build-javascript') {
-        actions.setWebpackConfig({
-            devtool: false
-        })
-    }
-};
